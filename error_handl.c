423f94af48d3710747f90363c389884409c9f826
Bright Olawale <oolatunjibright@yahoo.com>
2022-06-17 00:15:12 +0100

    [fix #3]: error function updated

--- error_handl.c
+++ error_handl.c
@@ -1,10 +1,41 @@
 #include "monty.h"
 
-void h_error(const char *message, unsigned int var_1, char *var_2)
+void erro(int code, ...)
 {
-	if (!var_1)
-		dprintf(STDERR_FILENO, message, var_2);
-	else
-		dprintf(STDERR_FILENO, message, var_1, var_2);
+    va_list param;
+
+    va_start(param, code);
+    switch (code)
+    {
+		case 1:
+			printf("USAGE: monty file\n");
+			break;
+		case 2:
+			printf("Error: Can't open file <file>\n", va_arg(param, char *));
+			break;
+		case 3:
+			printf("L%d: unknown instruction <opcode>\n", va_arg(param, int), va_arg(param, char *));
+			break;
+		case 4:
+			printf("Error: malloc failed\n");
+			break;
+		case 5:
+			printf("L%d: usage: push integer", va_arg(param, int));
+			break;
+		case 6:
+			printf("L%d: can't pint, stack empty", va_arg(param, int));
+			break;
+		case 7:
+			printf("L%d: can't pop an empty stack", va_arg(param, int));
+			break;
+		case 8:
+			printf("L%d: can't swap, stack too short", va_arg(param, int));
+			break;
+		case 9:
+			printf("L%d: can't add, stack too short", va_arg(param, int));
+			break;
+		default:
+			break;
+    }
 	exit(EXIT_FAILURE);
 }
