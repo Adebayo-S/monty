d9165fa6b39b29a3c1bd4ecb4adb693bad891324
Adebayo Samuel <eladebayoor@gmail.com>
2022-06-16 21:48:03 +0100

    [fix]: expanded scope

--- error_handl.c
+++ error_handl.c
@@ -1,7 +1,10 @@
 #include "monty.h"
 
-void h_error(char *message, char *variable)
+void h_error(const char *message, unsigned int var_1, char *var_2)
 {
-    dprintf(STDERR_FILENO, message, variable);
-    exit(EXIT_FAILURE);
+	if (!var_1)
+		dprintf(STDERR_FILENO, message, var_2);
+	else
+		dprintf(STDERR_FILENO, message, var_1, var_2);
+	exit(EXIT_FAILURE);
 }
