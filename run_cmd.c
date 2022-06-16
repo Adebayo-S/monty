025637d8d9b4473b2608509156907c298744f804
Adebayo Samuel <eladebayoor@gmail.com>
2022-06-16 21:50:19 +0100

    [feat]: run_cmd & get_cmd created and expanded

--- run_cmd.c
+++ run_cmd.c
@@ -1,11 +1,48 @@
 #include "monty.h"
 
-run_cmd(char *bufline)
+/**
+ * get_cmd - return the requisite opcode to run
+ * @cmd: opcode passed
+ * Return: pointer to the function that executes the opcode
+ */
+void (*get_cmd(void))(stack_t **head, unsigned int line_number)
 {
-	char *token;
-	char *delim = " \t\n";
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int l = 0;
+	int i;
+	instruction_t cmds[] = {
+		{"push", pusher},
+		{"pall", paller},
+		{"pint", pinter},
+		{"pop", popper},
+		/*{"swap", swapper},
+		{"add", adder},
+		{"nop", nopper},*/
+		{NULL, NULL}
+	};
+
+	for (i = 0; cmds[i].opcode; i++)
+	{
+		if (strcmp(cmds[i].opcode, var.cmd) == 0)
+			break;
+	}
+
+	return (cmds[i].f);
+}
+
+void run_cmd(char *bufline)
+{
+	void (*f)(stack_t **head, unsigned int line_number);
+
+	var.cmd = strtok(bufline, DELIM);
+	if (var.cmd && var.cmd[0] != '#')
+	{
+		f = get_cmd();
+		if (f)
+		{
+			var.value = strtok(NULL, DELIM);
+			f(&(var.head), var.line_number);
+		}
+		else
+			h_error(INVALID, var.line_number, var.cmd), free_stack();
+		free_var();
+	}
 }
