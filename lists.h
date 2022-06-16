ca0a307310697d64588251fdf48b4a04f9634b56
Adebayo Samuel <eladebayoor@gmail.com>
2022-06-16 21:48:30 +0100

    [feat]: linked lists header file created

---
+++ lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "monty.h"
+
+size_t print_dlistint(const stack_t *h);
+size_t dlistint_len(const stack_t *h);
+stack_t *add_dnodeint(stack_t **head, const int n);
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+void free_dlistint(stack_t *head);
+stack_t *get_dnodeint_at_index(stack_t *head, unsigned int index);
+int sum_dlistint(stack_t *head);
+stack_t *insert_dnodeint_at_index(stack_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(stack_t **head, unsigned int index);
+
+#endif
