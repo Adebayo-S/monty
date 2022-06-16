80fd22222ce3d9e5d5c5110445d33b4f5772f24c
Adebayo Samuel <eladebayoor@gmail.com>
2022-06-16 21:48:52 +0100

    [fix]: main.c expanded

--- main.c
+++ main.c
@@ -1,20 +1,49 @@
 #include "monty.h"
 
+global_t var;
+
+/**
+ * init_var - initializes the global variables
+ *
+ * Return: no return
+ */
+void init_var(void)
+{
+	var.bufline = NULL;
+	var.cmd = NULL;
+	var.value = NULL;
+	var.head = NULL;
+	var.line_number = 0;
+}
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
 int main(int argc, char **argv)
 {
-	FILE *fd;
 	size_t size = 0;
 
 	if (argc != 2)
-		h_error("USAGE: monty file\n", NULL);
+		h_error(USAGE, UNDEFINED, NULL);
 
-	fd = fopen(argv[1], "r") ? fopen(argv[1], "r") :
-		h_error("Error: Can't open file %s\n", argv[1]);
+	init_var();
 
-	while (getline(&var.bufline, &size, fd) != EOF)
+	var.fd = fopen(argv[1], "r");
+
+	if (!var.fd)
+		h_error(NOACCESS, UNDEFINED, argv[1]);
+
+	while (getline(&var.bufline, &size, var.fd) != EOF)
 	{
 		var.line_number++;
 		run_cmd(var.bufline);
 	}
+	free_var(), free_stack();
+	fclose(var.fd);
+
 	return (EXIT_SUCCESS);
 }
