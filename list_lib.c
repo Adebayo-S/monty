c889baea542be724f48fa586b5ad2a234e7b6c05
Bright Olawale <oolatunjibright@yahoo.com>
2022-06-16 11:12:23 +0100

    [feat

---
+++ list_lib.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+stack_t *makenode(int x)
+{
+    stack_t *newnode;
+
+    newnode = malloc(sizeof(stack_t));
+    if (!newnode)
+    {
+        h_error("Error: malloc failed", NULL);
+    }
+    newnode->prev = NULL;
+    newnode->n = x;
+    newnode->next = NULL;
+    
+    return (newnode);
+}
