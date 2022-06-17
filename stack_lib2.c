142b6e681edf99a4eb10f788355bc914aba17dc7
Adebayo Samuel <eladebayoor@gmail.com>
2022-06-17 02:17:15 +0100

    [style]: comments added

--- stack_lib2.c
+++ stack_lib2.c
@@ -1,21 +1,26 @@
 #include "monty.h"
 
+/**
+ * ivstg - investigate the operand to check if the input is alpha.
+ * @line_number: The line number of the opcode being worked on.
+ * @return: the operand
+ */
 char *ivstg(char *operand, unsigned int line_number)
 {
-    int k;
-    
-    if (operand == NULL)
-    {
-        fprintf(stderr, "L%d: usage: push integer", line_number);
-        exit(EXIT_FAILURE);
-    }
-    k = strlen(operand);
-    while (k--)
-    {
-        if (operand[k] < 47 && operand[k] < 58)
-            continue;
-        fprintf(stderr, "L%d: usage: push integer", line_number);
-        exit(EXIT_FAILURE);
-    }
-    return (operand);
+	int k;
+
+	if (operand == NULL)
+	{
+		fprintf(stderr, "0L%d: usage: push integer", line_number);
+		exit(EXIT_FAILURE);
+	}
+	k = strlen(operand);
+	while (k--)
+	{
+		if (operand[k] > 47 && operand[k] < 58)
+			continue;
+		fprintf(stderr, "1L%d: usage: push integer", line_number);
+		exit(EXIT_FAILURE);
+	}
+	return (operand);
 }
