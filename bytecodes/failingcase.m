00e9a5e8cf57f567741be775966de4e51e7720fd
Bright Olawale <oolatunjibright@yahoo.com>
2022-06-19 00:29:17 +0100

    enhancement

--- bytecodes/failingcase.m
+++ bytecodes/failingcase.m
@@ -1,5 +1,41 @@
+push 1 This should not be taken into account
+# Comment
+push 2
+push 3
+       # Another comment
+#
+#Above is an empty comment
+pint
+nop
+push 4
+swap
+pop
+ # Another just below
+      #
+add
+push 1
+sub
+push 2
+div
+push 12
+mul
+push 9
+mod
+push 0
+push 65
+pchar
+push 66
+push 67
+push 68
+pstr
+rotl
+push 87
+rotr
 queue
 push 98
-push 897
-stack Holberton
+push 1
+push 34
+push 344
+stack
+push 13
 pall
