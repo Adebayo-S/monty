8d430266ee4adbbe2dcd4f8068bc1c2c678ce4ee
Bright Olawale <oolatunjibright@yahoo.com>
2022-06-19 01:10:52 +0100

    enhancement

--- stack_lib.c
+++ stack_lib.c
@@ -17,14 +17,7 @@ void pusher(stack_t **stack, unsigned int line_number)
 	n = atoi(text);
 	if (var.mode == -1)
 	{
-		if (var.idx == 0)
-		{
-			add_dnodeint_end(stack, n);
-		}
-		else
-		{
-			insert_dnodeint_at_index(stack, (var.idx - 1), n);
-		}
+		add_dnodeint_end(stack, n);
 	}
 	else
 	{
